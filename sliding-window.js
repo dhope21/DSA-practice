85537cfe44071c4a64891c70707606b6363ed8b0
Abhijeet <dhope21@gmail.com>
2025-01-14 17:58:26 +0530

    adding smallest sub array with given sum

--- sliding-window.js
+++ sliding-window.js
@@ -1,22 +1,84 @@
-// actual approach
-const findAvgOfSubArr = (nums, k) => {
-  var window_sum = 0;
-  var start = 0;
-  const result = [];
-  let max_value = -Infinity;
-  for (end = 0; end < nums.length; end++) {
-    window_sum += nums[end];
-    if (end >= k - 1) {
-      console.log(start, end, window_sum);
-      result.push(window_sum / k);
-      max_value = Math.max(max_value,window_sum / k );
-      window_sum -= nums[start];
-      start++;
-    }
-  }
-  console.log(result, max_value);
-};
+// Smallest Subarray with a given sum (easy)
 
+var minSubArrayLen = function (nums, target) { 
+    let window_start = 0;
+    let sum_subarray = 0;
+    let min_subarray = Infinity;
+    for (let window_end = 0; window_end < nums.length; window_end ++ ){
+        sum_subarray += nums[window_end];
+        while (sum_subarray >= target){
+            min_subarray = Math.min(min_subarray, window_end - window_start);
+            sum_subarray -= nums[window_start]
+            window_start++
+        }
+    }
+    console.log(min_subarray) 
+}
+
+
+// first most obvious answer, it does not work in some cases.
+// var minSubArrayLen = function (nums, target) {
+//   let window_sum = 0;
+//   let window_first = 0;
+//   let min_length = Infinity;
+//   for (let window_last = 0; window_last < nums.length; window_last++) {
+//     const element = nums[window_last];
+//     if (window_sum < target) {
+//       window_sum += element;
+//       console.log("1", element, window_sum, window_last, window_first);
+//     } else {
+//       min_length = Math.min(min_length, window_last - window_first);
+//       window_sum -= nums[window_first];
+//       window_first++;
+//       window_last--;
+//       console.log("2", element, window_sum, window_last, window_first);
+//     }
+//     while (window_last == nums.length - 1 && window_sum > target) {
+//       min_length = Math.min(min_length, window_last - window_first);
+//       window_sum -= nums[window_first];
+//       window_first++;
+//       console.log("3", element, window_sum, window_last, window_first);
+//     }
+//   }
+//   if (min_length == Infinity) {
+//     min_length = 0;
+//   }
+//   console.log(window_sum, min_length);
+// };
+
+minSubArrayLen([2, 1, 5, 2, 3, 2], 7)
+// minSubArrayLen([1,1,1,1,1,1,1,1], 11)
+// minSubArrayLen([1,2,3,4,5], 11);
+
+
+
+
+// const example_array = [0, 4, 0, 3, 2];
+// const example_array = [1,12,-5,-6,50,3];
+
+// findAvgOfSubArr(example_array, 2);
+
+// Maximum Sum Subarray of Size K (easy)
+// actual approach
+// const findAvgOfSubArr = (nums, k) => {
+//   var window_sum = 0;
+//   var start = 0;
+//   const result = [];
+//   let max_value = -Infinity;
+//   for (end = 0; end < nums.length; end++) {
+//     window_sum += nums[end];
+//     if (end >= k - 1) {
+//       console.log(start, end, window_sum);
+//       result.push(window_sum / k);
+//       max_value = Math.max(max_value,window_sum / k );
+//       window_sum -= nums[start];
+//       start++;
+//     }
+//   }
+//   console.log(result, max_value);
+// };
+
+// Find Averages of Sub Arrays
 // first most obvious approach
 // function findAvgOfSubArr(nums, k) {
 //     let first, last = 0;
@@ -43,8 +105,3 @@ const findAvgOfSubArr = (nums, k) => {
 //     }
 //     console.log(result)
 // }
-
-const example_array = [0, 4, 0, 3, 2];
-// const example_array = [1,12,-5,-6,50,3];
-
-findAvgOfSubArr(example_array, 1);
